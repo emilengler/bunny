e237f640661d482b01eaa80d8e979ee8d08e8d9b
Emil Engler <me@emilengler.com>
2023-09-30 07:30:41 +0200

    crypto: Provide types

--- lib/crypto/crypto.ex
+++ lib/crypto/crypto.ex
@@ -1,9 +1,12 @@
 defmodule Bunny.Crypto do
+  @type hash :: binary()
+  @type key :: binary()
+
   @doc """
   A keyed hash function with one 32-byte input, one variable-size input, and one 32-byte output.
   As keyed hash function we use the HMAC construction with BLAKE2s as the inner hash function.
   """
-  @spec hash(binary(), binary()) :: binary()
+  @spec hash(key(), binary()) :: hash()
   def hash(key, data) do
     true = byte_size(key) == 32
     :crypto.mac(:hmac, :blake2s, key, data)
