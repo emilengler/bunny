a75fdbdb44dec1e56a5226797be084ab27ba7619
Emil Engler <me@emilengler.com>
2023-10-07 21:55:21 +0200

    config: Store the peers in a map
    This commit modifies the config module in a fashion, so that the peers
    get stored in a map, rather than a list, in order to provide O(1)
    instead of O(n) access.

--- lib/bunny/config.ex
+++ lib/bunny/config.ex
@@ -1,4 +1,5 @@
 defmodule Bunny.Config do
+  alias Bunny.Crypto
   alias Bunny.Config
   alias Bunny.Config.Peer
   alias Bunny.Crypto.SKEM
@@ -16,11 +17,25 @@ defmodule Bunny.Config do
   @type ip_address :: :inet.ip_address()
   @type host :: hostname() | ip_address()
   @type addrport :: {host(), :inet.port_number()}
+  @type peers :: map()
 
   @type t :: %Config{
           spkm: SKEM.public_key(),
           sskm: SKEM.secret_key(),
           listen: addrport() | nil,
-          peers: list(Peer)
+          peers: peers()
         }
+
+  @spec peers_from_list(list(Peer.t())) :: peers()
+  def peers_from_list(peers) do
+    # Converts peers into a tuple with {pidt, peer}
+    peers =
+      Enum.map(peers, fn peer ->
+        pidt = Crypto.lhash("peer id") |> Crypto.hash(peer.spkt)
+        {pidt, peer}
+      end)
+
+    # Converts the tuple into a map
+    Enum.into(peers, %{})
+  end
 end
